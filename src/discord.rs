eb21386097539420c364b3aff7b085f99770866b
H1ghBre4k3r <h1ghbre4k3r@dev.bre4k3r.de>
2022-07-02 16:32:30 +0200

    Comment the payload struct

--- src/discord.rs
+++ src/discord.rs
@@ -1,9 +1,14 @@
 use serde::{Deserialize, Serialize};
 
+/// Payload for communicating with the discord API
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Payload {
+    /// opcode for this payload
     op: i64,
+    /// event data
     d: Option<serde_json::Value>,
+    /// sequence number, used for resuming sessions and heartbeats
     s: Option<i64>,
+    /// the event name for this payload
     t: Option<String>,
 }
