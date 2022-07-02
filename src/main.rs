3f5318205ec63319c3da395f5d66b5fe1b1d6e36
H1ghBre4k3r <h1ghbre4k3r@dev.bre4k3r.de>
2022-07-02 17:25:46 +0200

    Switch websocket library

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,31 @@
 mod discord;
 
 use std::error::Error;
-use websocket::{native_tls::TlsConnector, ClientBuilder, OwnedMessage};
+use tungstenite::connect;
+use url::Url;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let mut client = ClientBuilder::new("wss://gateway.discord.gg/?v=10&encoding=json")?
-        .connect_secure(Some(TlsConnector::new()?))?;
-
-    for message in client.incoming_messages() {
-        let message = match message {
-            Ok(m) => m,
-            Err(e) => {
-                println!("Receive loop: {:?}", e);
-                return Ok(());
+    let (mut socket, _res) = connect(Url::parse("wss://gateway.discord.gg/?v=10&encoding=json")?)?;
+    loop {
+        let msg = socket.read_message()?;
+        match msg {
+            tungstenite::Message::Close(_) => {
+                break;
             }
-        };
-
-        match message {
-            websocket::OwnedMessage::Close(_) => {
-                client.shutdown()?;
-                return Ok(());
-            }
-            OwnedMessage::Text(msg) => {
+            tungstenite::Message::Text(msg) => {
                 let payload: discord::Payload = serde_json::from_str(msg.as_str())?;
                 println!("{:?}", payload);
             }
-            _ => println!("Receive Loop: {:?}", message),
-        }
+            tungstenite::Message::Ping(v) => {
+                match socket.write_message(tungstenite::Message::Pong(v)) {
+                    Err(e) => {
+                        panic!("Error writing to message: {}", e);
+                    }
+                    _ => (),
+                }
+            }
+            _ => panic!(),
+        };
     }
 
     Ok(())
