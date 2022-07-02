353cf3ab1b678512db2cdedce96997337b128359
H1ghBre4k3r <h1ghbre4k3r@dev.bre4k3r.de>
2022-07-02 16:41:03 +0200

    Add comments to GatewayOpcodes

--- src/discord.rs
+++ src/discord.rs
@@ -5,7 +5,7 @@ use serde_repr::{Deserialize_repr, Serialize_repr};
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Payload {
     /// opcode for this payload
-    op: Opcode,
+    op: GatewayOpcode,
     /// event data
     d: Option<serde_json::Value>,
     /// sequence number, used for resuming sessions and heartbeats
@@ -16,16 +16,27 @@ pub struct Payload {
 
 #[derive(Serialize_repr, Deserialize_repr, PartialEq, Debug)]
 #[repr(u8)]
-enum Opcode {
+enum GatewayOpcode {
+    /// An event was dispatched.
     Dispatch = 0,
+    /// Fired periodically by the client to keep the connection alive.
     Heartbeat = 1,
+    /// Starts a new session during the initial handshake.
     Identify = 2,
+    /// Update the clients presence.
     PresenceUpdate = 3,
+    /// Used to join/leave or move between voice channels.
     VoiceStateUpdate = 4,
+    /// Resume a previous session that was disconnected.
     Resume = 6,
+    /// You should attempt to reconnect and resume immediately.
     Reconnect = 7,
+    /// Request information about offline guild members in a large guild.
     RequestGuildMembers = 8,
+    /// The session has been invalidated. You should reconnect and identify/resume accordingly.
     InvalidSession = 9,
+    /// Sent immediatly after connecting. Contains the `heartbeat_interval` to use.
     Hello = 10,
+    /// Sent in response to receiving a heartbot to acknowledge that it has been received.
     HeartbeatACK = 11,
 }
