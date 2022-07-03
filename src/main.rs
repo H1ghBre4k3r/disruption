e8789c8f332d3daee6419e56863a9effbe4b703f
H1ghBre4k3r <h1ghbre4k3r@dev.bre4k3r.de>
2022-07-04 00:06:56 +0200

    Constantly loop and check for new messages

--- src/main.rs
+++ src/main.rs
@@ -1,80 +1,95 @@
 mod discord;
 
 use discord::{opcodes::GatewayOpcode, payloads::HelloPayloadData};
-use std::{error::Error, net::TcpStream};
-use tungstenite::{connect, stream::MaybeTlsStream, Message, WebSocket};
+use futures::{
+    stream::{SplitSink, SplitStream},
+    FutureExt, SinkExt,
+};
+use futures_util::StreamExt;
+use std::error::Error;
+use tokio::net::TcpStream;
+use tokio_tungstenite::{connect_async, tungstenite::Message, MaybeTlsStream, WebSocketStream};
 use url::Url;
 
 struct Client {
-    socket: WebSocket<MaybeTlsStream<TcpStream>>,
+    writer: SplitSink<WebSocketStream<MaybeTlsStream<TcpStream>>, Message>,
+    reader: SplitStream<WebSocketStream<MaybeTlsStream<TcpStream>>>,
     heartbeat: Option<u64>,
 }
 
 impl Client {
-    pub fn new() -> Result<Self, Box<dyn Error>> {
-        let (socket, _res) = connect(Url::parse("wss://gateway.discord.gg/?v=10&encoding=json")?)?;
+    pub async fn new() -> Result<Self, Box<dyn Error>> {
+        let (socket, _res) =
+            connect_async(Url::parse("wss://gateway.discord.gg/?v=10&encoding=json")?).await?;
+        let (writer, reader) = socket.split();
 
         Ok(Client {
-            socket,
+            writer,
+            reader,
             heartbeat: None,
         })
     }
 
-    pub fn start(&mut self) -> Result<(), Box<dyn Error>> {
-        self.init()?;
+    pub async fn start(&mut self) -> Result<(), Box<dyn Error>> {
+        self.init().await?;
         loop {
-            let msg = self.socket.read_message()?;
-            match msg {
-                Message::Close(r) => {
-                    println!("Closing connection: {:?}", r);
-                    break;
-                }
-                Message::Text(msg) => {
-                    let payload: discord::payloads::Payload = serde_json::from_str(msg.as_str())?;
-                    if let Err(e) = self.handle_payload(payload) {
-                        eprintln!("Error handling payload: {:?}", e);
-                    }
-                }
-                Message::Ping(v) => {
-                    match self.socket.write_message(tungstenite::Message::Pong(v)) {
-                        Err(e) => {
-                            panic!("Error writing to message: {}", e);
+            // loop and check for new messages
+            match self.reader.next().now_or_never() {
+                Some(Some(msg)) => {
+                    match msg? {
+                        Message::Close(r) => {
+                            println!("Closing connection: {:?}", r);
+                            break;
                         }
-                        _ => (),
-                    }
+                        Message::Text(msg) => {
+                            let payload: discord::payloads::Payload =
+                                serde_json::from_str(msg.as_str())?;
+                            println!("{:?}", payload);
+                            if let Err(e) = self.handle_payload(payload) {
+                                eprintln!("Error handling payload: {:?}", e);
+                            }
+                        }
+                        Message::Ping(v) => {
+                            self.writer.send(Message::Pong(v));
+                        }
+                        _ => panic!(),
+                    };
                 }
-                _ => panic!(),
-            };
+                _ => continue,
+            }
         }
 
         Ok(())
     }
 
-    fn init(&mut self) -> Result<(), Box<dyn Error>> {
-        self.handle_hello()?;
+    async fn init(&mut self) -> Result<(), Box<dyn Error>> {
+        self.handle_hello().await?;
         Ok(())
     }
 
-    fn handle_hello(&mut self) -> Result<(), Box<dyn Error>> {
-        let msg = self.socket.read_message()?;
-        match msg {
-            Message::Text(msg) => {
-                let payload: discord::payloads::Payload = serde_json::from_str(msg.as_str())?;
-                match payload.op {
-                    GatewayOpcode::Hello => match payload.d {
-                        Some(v) => {
-                            let hello_payload: HelloPayloadData = serde_json::from_value(v)?;
-                            self.heartbeat = Some(hello_payload.heartbeat_interval);
-                            self.heartbeating();
-                        }
-                        _ => {
-                            panic!("Gateway::Hello did not have matching payload")
-                        }
-                    },
-                    _ => panic!("Initial message from gateway was not Hello"),
+    async fn handle_hello(&mut self) -> Result<(), Box<dyn Error>> {
+        match self.reader.next().await {
+            None => panic!(""),
+            Some(msg) => match msg? {
+                Message::Text(msg) => {
+                    let payload: discord::payloads::Payload = serde_json::from_str(msg.as_str())?;
+                    match payload.op {
+                        GatewayOpcode::Hello => match payload.d {
+                            Some(v) => {
+                                let hello_payload: HelloPayloadData = serde_json::from_value(v)?;
+                                println!("{:?}", hello_payload);
+                                self.heartbeat = Some(hello_payload.heartbeat_interval);
+                                self.heartbeating();
+                            }
+                            _ => {
+                                panic!("Gateway::Hello did not have matching payload")
+                            }
+                        },
+                        _ => panic!("Initial message from gateway was not Hello"),
+                    }
                 }
-            }
-            _ => panic!("Unexpected message from gateway"),
+                _ => panic!("Unexpected message from gateway"),
+            },
         }
         Ok(())
     }
@@ -89,27 +104,29 @@ impl Client {
         Ok(())
     }
 
-    fn send(&mut self, payload: discord::payloads::Payload) -> Result<(), Box<dyn Error>> {
+    async fn send(&mut self, payload: discord::payloads::Payload) -> Result<(), Box<dyn Error>> {
         let msg = serde_json::to_string(&payload)?;
-        self.socket.write_message(Message::Text(msg))?;
+        self.writer.send(Message::Text(msg)).await?;
         Ok(())
     }
 
-    fn heartbeating(&mut self) {
+    async fn heartbeating(&mut self) {
         let payload = discord::payloads::Payload {
             op: GatewayOpcode::Heartbeat,
             d: None,
             s: None,
             t: None,
         };
-        if let Err(e) = self.send(payload) {
+        if let Err(e) = self.send(payload).await {
             panic!("{:?}", e)
         }
     }
 }
 
-fn main() -> Result<(), Box<dyn Error>> {
-    let mut client = Client::new()?;
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let mut client = Client::new().await?;
 
-    client.start()
+    client.start().await?;
+    Ok(())
 }
