417749aabb951ddecf81778b7a8bee6d6047f53f
H1ghBre4k3r <h1ghbre4k3r@dev.bre4k3r.de>
2022-07-06 00:03:29 +0200

    Add structs for IdentifiyPayload

--- src/discord/payloads.rs
+++ src/discord/payloads.rs
@@ -1,4 +1,5 @@
 use serde::{Deserialize, Serialize};
+use serde_repr::{Deserialize_repr, Serialize_repr};
 
 use super::opcodes::GatewayOpcode;
 
@@ -18,4 +19,68 @@ pub struct Payload {
 #[derive(Serialize, Deserialize, Debug)]
 pub struct HelloPayloadData {
     pub heartbeat_interval: u128,
+}
+
+/// ? https://discord.com/developers/docs/topics/gateway#identify
+#[derive(Serialize, Deserialize, Debug)]
+pub struct IdentifyPayloadData {
+    /// authentication token
+    pub token: String,
+    /// conenction properties
+    pub properties: IdentifyConnectionProperties,
+    /// whether thuis connectionsupport compression of packets
+    pub compress: Option<bool>,
+    /// value between 50 and 250, total number of members where the gateway will stop sending offline members in the guild member list
+    pub large_threshold: Option<u8>,
+    /// used for guild sharding
+    pub shard: Option<Vec<u64>>,
+    /// presence structure for initial presence information
+    pub presence: Option<PresenceUpdateStructure>,
+    /// the gateway intents you wish to receive
+    pub intents: u64,
+}
+
+/// ? https://discord.com/developers/docs/topics/gateway#identify-identify-connection-properties
+#[derive(Serialize, Deserialize, Debug)]
+pub struct IdentifyConnectionProperties {
+    /// your operating system
+    pub os: String,
+    /// your library name
+    pub browser: String,
+    /// your library name
+    pub device: String,
+}
+
+/// ? https://discord.com/developers/docs/topics/gateway#update-presence
+#[derive(Serialize, Deserialize, Debug)]
+pub struct PresenceUpdateStructure {
+    /// unix time (in ms) of when the client went idle if the client is not idle
+    pub since: Option<u128>,
+    /// the user's activities
+    pub activities: Vec<Activity>,
+    /// the user's new status
+    pub status: String,
+    /// whether or not the client is afk
+    pub afk: bool,
+}
+
+/// TODO: https://discord.com/developers/docs/topics/gateway#activity-object
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Activity {
+    pub name: String,
+    #[serde(rename = "type")]
+    pub type_: ActivityType,
+    pub url: Option<String>,
+}
+
+/// ? https://discord.com/developers/docs/topics/gateway#activity-object-activity-types
+#[derive(Serialize_repr, Deserialize_repr, PartialEq, Debug)]
+#[repr(u8)]
+pub enum ActivityType {
+    Game = 0,
+    Streaming = 1,
+    Listening = 2,
+    Watching = 3,
+    Custom = 4,
+    Competing = 5,
 }
