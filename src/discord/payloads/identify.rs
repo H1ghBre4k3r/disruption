712a39bf39d35f11ae227096a723aa1fee361cab
H1ghBre4k3r <h1ghbre4k3r@dev.bre4k3r.de>
2022-07-06 00:41:25 +0200

    Add Gateway Indents

--- src/discord/payloads/identify.rs
+++ src/discord/payloads/identify.rs
@@ -31,3 +31,63 @@ pub struct IdentifyConnectionProperties {
     /// your library name
     pub device: String,
 }
+
+#[allow(dead_code)]
+pub enum Indents {
+    #[allow(non_camel_case_types)]
+    GUILDS = 1 << 0,
+
+    #[allow(non_camel_case_types)]
+    GUILD_MEMBERS = 1 << 1,
+
+    #[allow(non_camel_case_types)]
+    GUILD_BANS = 1 << 2,
+
+    #[allow(non_camel_case_types)]
+    GUILD_EMOJIS_AND_STICKERS = 1 << 3,
+
+    #[allow(non_camel_case_types)]
+    GUILD_INTEGRATIONS = 1 << 4,
+
+    #[allow(non_camel_case_types)]
+    GUILD_WEBHOOKS = 1 << 5,
+
+    #[allow(non_camel_case_types)]
+    GUILD_INVITES = 1 << 6,
+
+    #[allow(non_camel_case_types)]
+    GUILD_VOICE_STATES = 1 << 7,
+
+    #[allow(non_camel_case_types)]
+    GUILD_PRESENCES = 1 << 8,
+
+    #[allow(non_camel_case_types)]
+    GUILD_MESSAGES = 1 << 9,
+
+    #[allow(non_camel_case_types)]
+    GUILD_MESSAGE_REACTIONS = 1 << 10,
+
+    #[allow(non_camel_case_types)]
+    GUILD_MESSAGE_TYPING = 1 << 11,
+
+    #[allow(non_camel_case_types)]
+    DIRECT_MESSAGES = 1 << 12,
+
+    #[allow(non_camel_case_types)]
+    DIRECT_MESSAGE_REACTIONS = 1 << 13,
+
+    #[allow(non_camel_case_types)]
+    DIRECT_MESSAGE_TYPING = 1 << 14,
+
+    #[allow(non_camel_case_types)]
+    MESSAGE_CONTENT = 1 << 15,
+
+    #[allow(non_camel_case_types)]
+    GUILD_SCHEDULED_EVENTS = 1 << 16,
+
+    #[allow(non_camel_case_types)]
+    AUTO_MODERATION_CONFIGURATION = 1 << 20,
+
+    #[allow(non_camel_case_types)]
+    AUTO_MODERATION_EXECUTION = 1 << 21,
+}
