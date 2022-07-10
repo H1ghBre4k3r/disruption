cc58cb317d340f1ae532a1ced4c8f12e2ce5870a
H1ghBre4k3r <h1ghbre4k3r@dev.bre4k3r.de>
2022-07-10 23:20:49 +0200

    Add AllowedMentions struct

--- src/discord/channel/mention.rs
+++ src/discord/channel/mention.rs
@@ -15,3 +15,16 @@ pub struct ChannelMention {
     /// the name of the channel
     pub name: String,
 }
+
+/// ? https://discord.com/developers/docs/resources/channel#allowed-mentions-object-allowed-mentions-structure
+#[derive(Serialize, Deserialize)]
+pub struct AllowedMentions {
+    /// An array of allowed mention types to parse from the content.
+    pub parse: Vec<String>,
+    /// Array of role_ids to mention (Max size of 100)
+    pub roles: Vec<String>,
+    /// Array of user_ids to mention (Max size of 100)
+    pub users: Vec<String>,
+    /// For replies, whether to mention the author of the message being replied to (default false)
+    pub replied_user: bool,
+}
