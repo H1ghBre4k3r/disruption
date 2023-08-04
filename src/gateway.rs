8b188d032f65a1022e473e96701b6dad47c5185a
H1ghBre4k3r <h1ghbre4k3r@dev.bre4k3r.de>
2023-08-04 17:56:35 +0200

    refactor(gateway): synchronously connect to gateway

--- src/gateway.rs
+++ src/gateway.rs
@@ -11,6 +11,7 @@ use disruption_types::{
     opcodes::GatewayOpcode,
     payloads::{HelloPayloadData, IdentifyConnectionProperties, IdentifyPayloadData, Payload},
 };
+use futures::executor::block_on;
 use futures_util::{
     stream::{SplitSink, SplitStream},
     SinkExt, StreamExt,
@@ -20,42 +21,39 @@ use tokio::{net::TcpStream, sync::Mutex, task::JoinHandle};
 use tokio_tungstenite::{connect_async, tungstenite::Message, MaybeTlsStream, WebSocketStream};
 use url::Url;
 
-pub struct Gateway {
-    token: String,
-    writer: Arc<
-        Mutex<
+type WriterLock = Arc<
+    Mutex<
+        Option<
             SplitSink<
                 WebSocketStream<MaybeTlsStream<TcpStream>>,
                 tokio_tungstenite::tungstenite::Message,
             >,
         >,
     >,
-    reader: Arc<Mutex<SplitStream<WebSocketStream<MaybeTlsStream<TcpStream>>>>>,
+>;
+
+type SocketReader = SplitStream<WebSocketStream<MaybeTlsStream<TcpStream>>>;
+
+pub struct Gateway {
+    token: String,
+    writer: WriterLock,
     /// Tuple containing sender and receiver for the channel receiving messages from the websocket
     rec_tuple: (Sender<Message>, Receiver<Message>),
-    heartbeat: Option<u128>,
-    last_heartbeat: Option<Arc<Mutex<SystemTime>>>,
     heartbeat_handle: Arc<Mutex<Option<JoinHandle<()>>>>,
+    receiver_handle: Option<JoinHandle<()>>,
 }
 
 impl Gateway {
     pub async fn connect(token: impl ToString) -> Result<Self, Box<dyn std::error::Error>> {
-        let (socket, _response) =
-            connect_async(Url::parse("wss://gateway.discord.gg/?v=10&encoding=json")?).await?;
-
-        let (writer, reader) = socket.split();
-
         // Stuff related to receiving messages from the websocket
         let rec_tuple = async_channel::unbounded::<Message>();
 
         let mut gateway = Gateway {
             token: token.to_string(),
-            writer: Arc::new(Mutex::new(writer)),
-            reader: Arc::new(Mutex::new(reader)),
+            writer: Arc::new(Mutex::new(None)),
             rec_tuple,
-            heartbeat: None,
-            last_heartbeat: None,
             heartbeat_handle: Arc::new(Mutex::new(None)),
+            receiver_handle: None,
         };
 
         gateway.spawn_receiver_thread().await?;
@@ -63,18 +61,40 @@ impl Gateway {
     }
 
     async fn spawn_receiver_thread(&mut self) -> Result<(), Box<dyn Error>> {
+        let token = self.token.clone();
         let writer_lock = self.writer.clone();
-        let reader_lock = self.reader.clone();
+        let heartbeat_handle_lock = self.heartbeat_handle.clone();
 
-        let (message_receiver, _) = self.rec_tuple.clone();
-        tokio::spawn(async move {
+        let (channel_writer, _) = self.rec_tuple.clone();
+        let receiver_handle = tokio::spawn(async move {
             loop {
-                loop {
-                    let mut reader = reader_lock.lock().await;
+                let (socket, _response) = connect_async(
+                    Url::parse("wss://gateway.discord.gg/?v=10&encoding=json")
+                        .expect("could not parse URL"),
+                )
+                .await
+                .expect("could not connect to gateway");
 
-                    match reader.next().await {
+                let (socket_writer, mut socket_reader) = socket.split();
+                {
+                    let mut writer_inner = writer_lock.lock().await;
+                    *writer_inner = Some(socket_writer);
+                }
+
+                Self::connect_to_gateway(
+                    token.clone(),
+                    &mut socket_reader,
+                    writer_lock.clone(),
+                    heartbeat_handle_lock.clone(),
+                )
+                .await;
+
+                loop {
+                    debug!("starting loooook");
+                    match socket_reader.next().await {
                         Some(Ok(message)) => {
-                            if let Err(e) = message_receiver.send(message).await {
+                            debug!("got message");
+                            if let Err(e) = channel_writer.send(message).await {
                                 trace!("[{}:{}] {}", file!(), line!(), e);
                             }
                         }
@@ -84,39 +104,66 @@ impl Gateway {
                         None => break,
                     }
                 }
-                // TODO: stop heartbeat and reconnect
-
-                let (socket, _response) = connect_async(
-                    Url::parse("wss://gateway.discord.gg/?v=10&encoding=json")
-                        .expect("could not parse URL"),
-                )
-                .await
-                .expect("could not connect to gateway");
-
-                let (writer, reader) = socket.split();
-                let mut writer_inner = writer_lock.lock().await;
-                let mut reader_inner = reader_lock.lock().await;
-
-                *writer_inner = writer;
-                *reader_inner = reader;
             }
         });
-        self.handle_hello().await?;
-        self.identify().await
+        self.receiver_handle = Some(receiver_handle);
+        Ok(())
+    }
+
+    async fn connect_to_gateway(
+        token: String,
+        socket_reader: &mut SocketReader,
+        writer_lock: WriterLock,
+        heartbeat_handle_lock: Arc<Mutex<Option<JoinHandle<()>>>>,
+    ) {
+        if let Err(e) = Self::handle_hello(socket_reader, &writer_lock, heartbeat_handle_lock).await
+        {
+            panic!("{e}");
+        }
+
+        if let Err(e) = Self::identify(&token, &writer_lock).await {
+            panic!("{e}");
+        };
+        let msg = Self::static_receive(socket_reader).await;
+        debug!("{msg:?}");
+        debug!("identify successful!");
     }
 
     pub async fn send(
         &self,
         payload: disruption_types::payloads::Payload,
     ) -> Result<(), Box<dyn Error>> {
-        let message = serde_json::to_string(&payload)?;
-        self.writer
-            .lock()
-            .await
-            .send(Message::Text(message))
-            .await?;
+        Self::static_send(&self.writer, payload).await
+    }
 
+    pub async fn static_send(
+        writer: &WriterLock,
+        payload: disruption_types::payloads::Payload,
+    ) -> Result<(), Box<dyn Error>> {
+        let message = serde_json::to_string(&payload)?;
+
+        debug!("static_send {message}");
+        if let Some(writer) = writer.lock().await.as_mut() {
+            debug!("sending...");
+            writer.send(Message::Text(message)).await?;
+            debug!("Done!");
+        } else {
+            error!("uuupsie");
+        }
+        debug!("successfully send");
         Ok(())
+    }
+
+    async fn static_receive(socket_reader: &mut SocketReader) -> Message {
+        debug!("static_receive");
+        match socket_reader.next().await {
+            Some(Ok(message)) => message,
+            Some(Err(e)) => {
+                error!("Error reading from socket: {e}");
+                panic!()
+            }
+            None => panic!(),
+        }
     }
 
     pub async fn receiver(&self) -> &Receiver<Message> {
@@ -124,40 +171,44 @@ impl Gateway {
     }
 
     /// Handle the intial message after connecting to the discord gateway.
-    async fn handle_hello(&mut self) -> Result<(), Box<dyn Error>> {
-        let (_, mut reader) = self.rec_tuple.clone();
-        match reader.next().await {
-            None => panic!(""),
-            Some(msg) => match msg {
-                Message::Text(msg) => {
-                    let payload: disruption_types::payloads::Payload =
-                        serde_json::from_str(msg.as_str())?;
-                    match payload.op {
-                        GatewayOpcode::Hello => match payload.d {
-                            Some(v) => {
-                                let hello_payload: HelloPayloadData = serde_json::from_value(v)?;
-                                self.heartbeat = Some(hello_payload.heartbeat_interval);
-                                self.last_heartbeat = Some(Arc::new(Mutex::new(SystemTime::now())));
-                                self.start_heartbeating().await;
-                            }
-                            _ => {
-                                panic!("Gateway::Hello did not have matching payload")
-                            }
-                        },
-                        _ => panic!("Initial message from gateway was not Hello"),
-                    }
+    async fn handle_hello(
+        socket_reader: &mut SocketReader,
+        writer_lock: &WriterLock,
+        heartbeat_handle_lock: Arc<Mutex<Option<JoinHandle<()>>>>,
+    ) -> Result<(), Box<dyn Error>> {
+        match Self::static_receive(socket_reader).await {
+            Message::Text(msg) => {
+                let payload: disruption_types::payloads::Payload =
+                    serde_json::from_str(msg.as_str())?;
+                match payload.op {
+                    GatewayOpcode::Hello => match payload.d {
+                        Some(v) => {
+                            let hello_payload: HelloPayloadData = serde_json::from_value(v)?;
+                            debug!("hello payload: {hello_payload:?}");
+                            Self::start_heartbeating(
+                                hello_payload.heartbeat_interval,
+                                writer_lock.clone(),
+                                heartbeat_handle_lock,
+                            )
+                            .await;
+                        }
+                        _ => {
+                            panic!("Gateway::Hello did not have matching payload")
+                        }
+                    },
+                    _ => panic!("Initial message from gateway was not Hello"),
                 }
-                _ => panic!("Unexpected message from gateway"),
-            },
+            }
+            _ => panic!("Unexpected message from gateway"),
         }
         Ok(())
     }
 
     /// Identify to the gateway.
-    async fn identify(&mut self) -> Result<(), Box<dyn Error>> {
+    async fn identify(token: &str, writer_lock: &WriterLock) -> Result<(), Box<dyn Error>> {
         info!("Trying to identify with Gateway");
         let payload_data = IdentifyPayloadData {
-            token: self.token.clone(),
+            token: token.to_owned(),
             properties: IdentifyConnectionProperties {
                 os: "linux".to_owned(),
                 browser: "disruption".to_owned(),
@@ -178,17 +229,19 @@ impl Gateway {
             ..Default::default()
         };
 
-        self.send(payload).await?;
+        Self::static_send(writer_lock, payload).await?;
 
         Ok(())
     }
 
     /// Start heartbeating with the API.
-    async fn start_heartbeating(&mut self) {
+    async fn start_heartbeating(
+        heartbeat_interval: u128,
+        writer_lock: WriterLock,
+        heartbeat_handle_lock: Arc<Mutex<Option<JoinHandle<()>>>>,
+    ) {
         info!("Starting heartbeat...");
-        let heartbeat_interval = self.heartbeat.unwrap();
 
-        let writer_lock = self.writer.clone();
         let heartbeat_handle = tokio::spawn(async move {
             loop {
                 thread::sleep(Duration::from_millis(heartbeat_interval as u64));
@@ -204,17 +257,29 @@ impl Gateway {
                 };
 
                 debug!("Sending heartbeat...");
-                let msg = serde_json::to_string(&payload).unwrap();
-                if let Err(e) = writer_lock.lock().await.send(Message::Text(msg)).await {
+                if let Err(e) = Self::static_send(&writer_lock, payload).await {
                     panic!("Error sending heartbeat ({})", e);
                 }
+                debug!("Send heartbeat!");
             }
         });
 
-        let mut current_hearbeat = self.heartbeat_handle.lock().await;
+        let mut current_hearbeat = heartbeat_handle_lock.lock().await;
         if current_hearbeat.is_some() {
             current_hearbeat.as_ref().unwrap().abort();
         }
         *current_hearbeat = Some(heartbeat_handle);
     }
 }
+
+// impl Drop for Gateway {
+//     fn drop(&mut self) {
+//         if let Some(receiver_handle) = &self.receiver_handle {
+//             receiver_handle.abort();
+//         }
+//         let heartbeat_handle = self.heartbeat_handle.blocking_lock();
+//         if heartbeat_handle.is_some() {
+//             heartbeat_handle.as_ref().unwrap().abort();
+//         }
+//     }
+// }
